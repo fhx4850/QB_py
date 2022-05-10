6873558d39ed9a38964b060f10718ae6982407b1
fhx <fhx4850@gmail.com>
2022-05-10 21:16:15 +0200

    refactor server.py code

--- QB/server/server.py
+++ QB/server/server.py
@@ -3,24 +3,18 @@ from urls import url_path
 from QB.rout.url import UrlRouting
 import QB.settings as settings
 
-class IpTcpServer:
-    """Server implementation."""
 
-    def __init__(self, ip: str, port: int):
-        self._serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._serv_socket.bind((ip, port))
-        UrlRouting(url_path)
+class _ServerResponce:
+    def __init__(self, request):
+        self.responce = None
         self._URLS = UrlRouting.URLS
-        self.dd = ""
+        self._create_response(request)
 
-    def run(self):
-        self._serv_listen()
+    def get_responce(self):
+        return self.responce
 
-    def _serv_listen(self):
-        self._serv_socket.listen()
-        while True:
-            client, addr = self._serv_socket.accept()
-            self._client_thread(client)
+    def set_responce(self, responce):
+        self.responce = responce
 
     def _create_response(self, request):
         """The method collects all the necessary data to respond to the user."""
@@ -29,49 +23,18 @@ class IpTcpServer:
         header, code = self._create_headers(method, url)
         body = self._create_body(code, url)
         if file_path and file_path != 'resources':
-            file = self.render_file(file_path)
-            return file
+            file = self._render_file(file_path)
+            self.set_responce(file)
         else:
-            return (header + body).encode()
-
-    def render_file(self, file_path):
-        """Processing files for transmission in response."""
-        with open(file_path, 'rb') as file:
-            response = file.read()
-
-        header = 'HTTP/1.1 200 OK\n'
-
-        # Determining the file extension.
-        if file_path.endswith(".jpg"):
-            mimetype = 'image/jpg'
-        elif file_path.endswith(".css"):
-            mimetype = 'text/css'
-        else:
-            mimetype = 'text/html'
-
-        header += 'Content-Type: ' + str(mimetype) + '\n\n'
-
-        final_response = header.encode('utf-8')
-        final_response += response
-
-        return final_response
-
-    # def open_data(self):
-    #     f = open('text.txt', 'r')
-    #     a = f.read()
-    #     self.dd = a
-    #     f.close()
+            self.set_responce((header + body).encode())
 
     def _get_request_string(self, request):
         """Splits the string data of the client request into (method, url, file_path)."""
         parsed = request.split(' ')
-
         # Web request method.
         method = parsed[0]
-
         # Request addres
         url = parsed[1]
-
         file_path = None
         if url.find('.') != -1:
             path = settings.RESOURCEDIR + str(url).partition(settings.RESOURCEDIR)[2]
@@ -98,6 +61,52 @@ class IpTcpServer:
         else:
             return self._URLS[url]()
 
+    def _render_file(self, file_path):
+        """Processing files for transmission in response."""
+        with open(file_path, 'rb') as file:
+            response = file.read()
+
+        header = 'HTTP/1.1 200 OK\n'
+
+        # Determining the file extension.
+        if file_path.endswith(".jpg"):
+            mimetype = 'image/jpg'
+        elif file_path.endswith(".css"):
+            mimetype = 'text/css'
+        else:
+            mimetype = 'text/html'
+
+        header += 'Content-Type: ' + str(mimetype) + '\n\n'
+
+        final_response = header.encode('utf-8')
+        final_response += response
+
+        return final_response
+
+    def _find_file(self, path):
+        myfile = path.lstrip('/')
+        return myfile
+
+
+class IpTcpServer:
+    """Server implementation."""
+
+    def __init__(self, ip: str, port: int):
+        self._serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._serv_socket.bind((ip, port))
+        UrlRouting(url_path)
+        self._URLS = UrlRouting.URLS
+        self.dd = ""
+
+    def run(self):
+        self._serv_listen()
+
+    def _serv_listen(self):
+        self._serv_socket.listen()
+        while True:
+            client, addr = self._serv_socket.accept()
+            self._client_thread(client)
+
     def output_request_log(self, request):
         request_url = request.decode('utf-8')
         print(str(request_url).splitlines()[0])
@@ -106,12 +115,9 @@ class IpTcpServer:
         """Processing client requests."""
         while True:
             request = client.recv(4048)
-            response = self._create_response(request)
+            sr = _ServerResponce(request)
+            response = sr.get_responce()
             client.send(response)
             client.close()
             break
         self.output_request_log(request)
-
-    def _find_file(self, path):
-        myfile = path.lstrip('/')
-        return myfile
